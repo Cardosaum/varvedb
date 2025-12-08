820effd09ab9d72ecb3790c1c32803caa169aa85
Matheus <gh@matheus.sbs>
2025-12-08 14:07:04 -0300

    style: fix admonition rendering and update landing page docs link

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import remarkGithubBlockquoteAlert from 'remark-github-blockquote-alert';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
   site: 'https://varvedb.com',
   compressHTML: true,
+  markdown: {
+    remarkPlugins: [
+      remarkGithubBlockquoteAlert,
+      remarkGfm,
+    ],
+  },
 });
