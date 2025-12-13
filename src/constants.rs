b830fd2597e48440081bc2cc84445a935056de51
Matheus <gh@matheus.sbs>
2025-12-13 00:34:43 -0300

    chore: Add licences

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,11 @@
+// This file is part of VarveDB.
+//
+// Copyright (C) 2025 Matheus Cardoso <varvedb@matheus.sbs>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+
 pub const EVENTS_DB_NAME: &str = "events";
 pub const DEFAULT_MAP_SIZE: usize = 10 * 1024 * 1024; // 10 MB
 pub const DEFAULT_MAX_DBS: u32 = 1;
